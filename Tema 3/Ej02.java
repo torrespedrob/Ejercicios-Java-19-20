d17c9af5c4d0dfff0c72a62f1e1f04595e053816
Pedro Torres <torrespedrobarba@gmail.com>
2019-09-30 11:17:23 +0200

    Añadidos nuevos ejercicios

--- Tema 3/Ej02.java
+++ Tema 3/Ej02.java
@@ -2,9 +2,9 @@ public class Ej02 {
   public static void main(String[] args) {
     System.out.println("Conversor de euros a pesetas");
     System.out.println("Introduce la cantidad en euros: ");
-    int numero1 = Integer.parseInt(System.console().readLine());
+    double numero1 = Double.parseDouble(System.console().readLine());
     double peseta = 166.386;
     double calculo = numero1 * peseta;
-    System.out.println(numero1 + " euros son " + calculo + " pesetas");
+    System.out.printf(numero1 + " euros son " + (int) calculo + " pesetas");
   }
 }
