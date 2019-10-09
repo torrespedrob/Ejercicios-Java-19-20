1b9cbd0beaf3474f1ad234b6ec9696d5fd26dffa
Pedro Torres <torrespedrobarba@gmail.com>
2019-10-09 10:32:18 +0200

    Añadido un ejercicio del tema 5

---
+++ Tema 4/Ej26.java
@@ -0,0 +1,12 @@
+public class Ej26 {
+  public static void main(String[] args) {
+    System.out.println("Venta de entradas CineCampa");
+    System.out.println("Introduce un número de entradas: ");
+    int numEntradas = Integer.parseInt(System.console().readLine());
+    System.out.println("Introduce el día de la semana: ");
+    String dia = System.console().readLine();
+    System.out.println("¿Tiene tarjeta CineCampa?: ");
+    
+    
+  }
+}
---
+++ Tema 5/ej01.java
@@ -0,0 +1,13 @@
+public class ej01 {
+  public static void main(String[] args) {
+    System.out.println("Múltiplos de 5");
+    int numero = 0;
+    
+    for (int i = 0; i < 101; i++) {
+    System.out.println(numero);
+    numero += 5;
+    }
+    
+    
+  }
+}
