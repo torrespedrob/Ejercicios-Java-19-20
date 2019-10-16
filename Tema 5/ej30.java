695a137d4d976054885fdc6344646d7706b13373
Pedro Torres <torrespedrobarba@gmail.com>
2019-10-16 11:15:55 +0200

    Añadidos más ejercicios del tema 5

--- Tema 5/ej30.java
+++ Tema 5/ej30.java
@@ -1,7 +1,22 @@
-public class ej28 {
+public class ej30 {
   public static void main(String[] args) {
-    System.out.println("Puedo calcular el factorial de un número introducido");
+    System.out.println("Puedo calcular las horas de un día a otro");
+    System.out.println("Por favor, introduzca la primera hora");
+    System.out.print("Día(1 a 7): ");
+    int dia = Integer.parseInt(System.console().readLine());
+    System.out.print("Hora(0 a 23): ");
+    int hora = Integer.parseInt(System.console().readLine());
+    boolean datosCorrectos = true;
     
+    System.out.println("Por favor, introduzca la segunda hora");
+    int dia2 = Integer.parseInt(System.console().readLine());
+    System.out.print("Hora(0 a 23): ");
+    int hora2 = Integer.parseInt(System.console().readLine());
+    
+    //Cálculo
+    do {
+      
+    } while (!datosCorrectos)
     
   }
 }
