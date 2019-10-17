2e7cf9c542ef376cc35c5c51edcec286798c5e06
Pedro Torres <torrespedrobarba@gmail.com>
2019-10-17 10:15:32 +0200

    Añadidos más ejercicios del tema 5

--- Tema 5/Sorpresa1.java
+++ Tema 5/Sorpresa1.java
@@ -1,28 +1,28 @@
 public class Sorpresa1 {
-
   public static void main(String[] args) {
-
+    //Entrada
     System.out.print("Por favor, introduzca la altura del reloj de arena: ");
     int alturaIntroducida = Integer.parseInt(System.console().readLine());
-
+    System.out.print("Introduce el carácter del reloj: ");
+    String caracter = System.console().readLine();
+    System.out.print("Introduce el número de repeticiones: ");
+    int repeticiones = Integer.parseInt(System.console().readLine());
+    System.out.println();
+    
+    //Cálculo
+    for (int z = 0; z < repeticiones; z++) {
     int altura = 1;
     int asteriscos = alturaIntroducida;
     int espaciosPorDelante = 0;
-    
-
     if ((alturaIntroducida < 3) || (alturaIntroducida % 2 == 0)) {
       System.out.print("Datos incorrectos. Debe introducir una altura impar mayor o igual a 3");
     } else {
-      System.out.print("Introduzca el carácter del reloj de arena: ");
-      String caracter = System.console().readLine();
-      // parte de arriba /////////////////////////////////////
+      // parte de arriba //
       while (altura < alturaIntroducida / 2 + 1) {
-        
         // inserta espacios delante
         for (int i = 1; i <= espaciosPorDelante; i++) {
           System.out.print(" ");
         }
-        
         // pinta la línea
         for (int i = 0; i < asteriscos; i++) {
           System.out.print(caracter);
@@ -32,9 +32,9 @@ public class Sorpresa1 {
         altura++;
         espaciosPorDelante++;
         asteriscos -= 2;      
-      } // while parte de arriba ///////////////////////
+      } // while parte de arriba //
       
-      // parte de abajo /////////////////////////////////////
+      // parte de abajo //
       espaciosPorDelante = alturaIntroducida / 2;
       altura = 1;
       while (altura <= alturaIntroducida / 2 + 1) {
@@ -53,7 +53,10 @@ public class Sorpresa1 {
         altura++;
         espaciosPorDelante--;
         asteriscos += 2;
-      } // while parte de abajo ///////////////////////
-    } // else
+        
+      } System.out.println();// while parte de abajo //
+    }
+    }
+    
   }
 }
