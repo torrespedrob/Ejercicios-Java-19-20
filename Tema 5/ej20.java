5c6099d8d45d8582f40eff7c6ee8b69e61bc31a6
Pedro Torres <torrespedrobarba@gmail.com>
2019-10-17 09:08:24 +0200

    Añadidos más ejercicios del tema 5

--- Tema 5/ej20.java
+++ Tema 5/ej20.java
@@ -1,7 +1,26 @@
-public class ej20 {
+public class ej19 {
   public static void main(String[] args) {
     System.out.println("Puedo dibujar pirámides");
-    System.out.print("Introduce el caracter con el que dibujar la piramide: ");
+    System.out.print("Introduce la altura de la piramide: ");
+    int altura = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduce el carácter de la piramide: ");
+    String caracter = System.console().readLine();    
+    int caracteresPorFila = 1;
+    int espaciosDelanteros = altura;
+    
+    //Cálculo
+    for (int i  = 0; i < altura; i++) {
+      //Espacios delanteros en cada fila
+      for (int j = 0; j < espaciosDelanteros; j++ ) {
+        System.out.print(" ");
+      }
+      //Pinta la fila
+      for (int j = 0; j < caracteresPorFila ; j++) {
+        System.out.print(caracter);
+      }
+      System.out.println();
+      caracteresPorFila += 2;
+      espaciosDelanteros--;
     }
   }
 }
---
+++ Tema 5/ej25.java
@@ -0,0 +1,12 @@
+public class ej25 {
+  public static void main(String[] args) {
+    System.out.println("Puedo saber los múltiplos de 3 hasta un número introducido");
+    int numero = Integer.parseInt(System.console().readLine());
+    
+    for (int i = 3; i < numero; i++) {
+      if ((i % 3) == 0) {
+        System.out.println("El número " + i + " es múltiplo de 3");
+      }
+    }
+  }
+}
