bd34a491d6d058aed28ff0f7ec934213ac1c60d0
Pedro Torres <torrespedrobarba@gmail.com>
2019-10-31 10:01:42 +0100

    Añadido un ejercicio del tema 6

---
+++ Tema 6/Ej23.java
@@ -0,0 +1,119 @@
+public class Ej23 { // Clase principal
+  public static void main(String[] args) {
+    //Creacion de variables para aleatorizacion
+    int dado1;
+    int dado2;
+    int dado3;
+    int dado4;
+    int dado5;
+    String salida;
+    
+    //Aleatorizacion
+    dado1 = (int)(Math.random() * 6);
+    dado2 = (int)(Math.random() * 6);
+    dado3 = (int)(Math.random() * 6);
+    dado4 = (int)(Math.random() * 6);
+    dado5 = (int)(Math.random() * 6);
+    
+    switch (dado1) {
+      case 0:
+        System.out.print("As ");
+      break;
+      case 1:
+        System.out.print("K ");
+      break;
+      case 2:
+        System.out.print("Q ");
+      break;
+      case 3:
+        System.out.print("J ");
+      break;
+      case 4:
+        System.out.print("7 ");
+      break;
+      case 5:
+        System.out.print("8 ");
+      break;
+    }
+    switch (dado2) {
+      case 0:
+        System.out.print("As ");
+      break;
+      case 1:
+        System.out.print("K ");
+      break;
+      case 2:
+        System.out.print("Q ");
+      break;
+      case 3:
+        System.out.print("J ");
+      break;
+      case 4:
+        System.out.print("7 ");
+      break;
+      case 5:
+        System.out.print("8 ");
+      break;
+    }
+    switch (dado3) {
+      case 0:
+        System.out.print("As ");
+      break;
+      case 1:
+        System.out.print("K ");
+      break;
+      case 2:
+        System.out.print("Q ");
+      break;
+      case 3:
+        System.out.print("J ");
+      break;
+      case 4:
+        System.out.print("7 ");
+      break;
+      case 5:
+        System.out.print("8 ");
+      break;
+    }
+    switch (dado4) {
+      case 0:
+        System.out.print("As ");
+      break;
+      case 1:
+        System.out.print("K ");
+      break;
+      case 2:
+        System.out.print("Q ");
+      break;
+      case 3:
+        System.out.print("J ");
+      break;
+      case 4:
+        System.out.print("7 ");
+      break;
+      case 5:
+        System.out.print("8 ");
+      break;
+    }
+    switch (dado5) {
+      case 0:
+        System.out.print("As ");
+      break;
+      case 1:
+        System.out.print("K ");
+      break;
+      case 2:
+        System.out.print("Q ");
+      break;
+      case 3:
+        System.out.print("J ");
+      break;
+      case 4:
+        System.out.print("7 ");
+      break;
+      case 5:
+        System.out.print("8 ");
+      break;
+    }
+  }
+}
