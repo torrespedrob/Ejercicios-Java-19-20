7a2e3161017f1c02488dcc0e612abaaea831a7a9
Pedro Torres <torrespedrobarba@gmail.com>
2019-11-04 10:41:42 +0100

    Añadido un ejercicio del tema 6

--- Tema 6/Ej24.java
+++ Tema 6/Ej24.java
@@ -1,5 +1,21 @@
 public class Ej24 { // Clase principal
   public static void main(String[] args) {
-    System.out.println("")
+    //Entrada
+    System.out.println("Puedo escoger un digito aleatorio de un numero");
+    System.out.print("Introduce un numero entero: ");
+    int entrada = Integer.parseInt(System.console().readLine());
+    int digito = entrada;
+    int posicion;
+    int longitud = 0;
+    
+    //Programa
+    do {
+      digito /= 10;
+      longitud++;
+    } while (digito > 0);
+    
+    posicion = (int)(Math.random() * longitud) + 1;
+    System.out.print((entrada / (long)(Math.pow(10, longitud - posicion))) % 10);
+    
   }
 }
