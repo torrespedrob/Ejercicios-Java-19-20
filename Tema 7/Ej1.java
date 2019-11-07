8a4ca07182fbe5d9d35553d0cebfebd8cce237c8
Pedro Torres <torrespedrobarba@gmail.com>
2019-11-07 09:29:21 +0100

    Comienzo del tema 7

---
+++ Tema 7/Ej1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Ej1 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int[] num;
+    num = new int[10];
+    num[0] = 39;
+    num[1] = -2;
+    num[4] = 0;
+    num[6] = 16;
+    num[8] = 5;
+    num[9] = 120;
+    
+    System.out.println(" " + num);
+  }
+}
