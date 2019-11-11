08c2ffdd250467b4ac111453b5e939764514199d
Pedro Torres <torrespedrobarba@gmail.com>
2019-11-11 08:42:57 +0100

    Añadido un ejercicio del tema 7

--- Tema 7/Ej04.java
+++ Tema 7/Ej04.java
@@ -1,7 +1,21 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Ej04 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
+    int[] numero = new int[20];
+    int[] cuadrado = new int[20];
+    int[] cubo = new int[20];
+    
+    for (int i =0; i< 20; i++) {
+      numero[i] = (int)(Math.random() * 100);
+      cuadrado[i] = (int)(i * i);
+      cubo[i] = (int)(i * i * i);
+    }
+    
+    System.out.println(Arrays.toString(numero));
+    System.out.println(Arrays.toString(cuadrado));
+    System.out.println(Arrays.toString(cubo));
     
   }
 }
