4a7c16fe3c42386126d58c52077b0ececed2d660
Pedro Torres <torrespedrobarba@gmail.com>
2019-11-14 09:19:37 +0100

    Añadido un ejercicio del tema 7

---
+++ Tema 7/Ej16.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Ej16 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Array de 20 elementos, podras elegir entre multiplos de 5 o 7");
+    int[] generado = new int[20];
+    
+  }
+}
