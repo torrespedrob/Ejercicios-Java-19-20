ca8404a3b86293005a10291259810cce8875b0a7
Pedro Torres <torrespedrobarba@gmail.com>
2019-11-14 09:20:14 +0100

    Añadido un ejercicio del tema 7

---
+++ Tema 7/Ej14.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Ej14 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    String[] color = {
+      "rojo", "naranja", "amarillo", "verde", "azul",  "morado", "rosa", "blanco", "negro"
+    };
+    String entrada[] = new String[8];
+    String resultado[] = new String[8];
+    
+    //Va rellenando el array
+    for (int i = 0; i < 8; i++) {
+      entrada[i]  = s.nextLine();
+    }
+   }
+}
