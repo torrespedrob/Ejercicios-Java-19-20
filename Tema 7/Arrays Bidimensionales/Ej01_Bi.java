50215f39c302ffba20d43745a6607042abe8cdc0
Pedro Torres <torrespedrobarba@gmail.com>
2019-11-18 10:27:53 +0100

    Añadido un ejercicio de arrays bidimensionales

---
+++ Tema 7/Arrays Bidimensionales/Ej01_Bi.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Ej01_Bi {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int[][] num = new int [3][6];
+    
+    num[0][0]=0;
+    num[1][0]=75;
+    num[0][1]=30;
+    num[0][2]=2;
+    num[2][2]=-2;
+    num[2][3]=9;
+    num[1][4]=0;
+    num[0][5]=5;
+    num[2][5]=11;
+    
+    int fila;
+    int columna;
+    
+    for (fila = 0; fila < 3; fila++) {
+      System.out.print("Fila: " + fila);
+      
+      for (columna = 0; columna < 6; columna++) {
+        System.out.printf("%10d ", num[fila][columna]);
+      }
+      System.out.println();
+    }
+  }
+}
