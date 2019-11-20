e0ca4c906dfa8abda24755a06fb96d42310014f9
Pedro Torres <torrespedrobarba@gmail.com>
2019-11-20 10:09:31 +0100

    Añadido un ejercicio de arrays bidimensionales

--- Tema 7/Arrays Bidimensionales/Ej08_Bi.java
+++ Tema 7/Arrays Bidimensionales/Ej08_Bi.java
@@ -3,5 +3,17 @@ public class Ej08_Bi {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
+    //Declaracion
+    int[][] array = new int[8][8];
+    String entrada;
+    final int mov;
+    final int casilla;
+    
+    //Juego
+    System.out.print("Introduzca la posición del alfil: ");
+    entrada = s.nextLine();
+    
+    
+    
   }
 }
---
+++ Tema 7/Arrays Bidimensionales/Ej09_Bi.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Ej09_Bi {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+  }
+}
