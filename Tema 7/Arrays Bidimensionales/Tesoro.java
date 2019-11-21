dce84ea8bc06341f8a3c8b4cdfc4e329400b7e8f
Pedro Torres <torrespedrobarba@gmail.com>
2019-11-21 08:40:34 +0100

    Añadido un ejercicio de arrays bidimensionales

--- Tema 7/Arrays Bidimensionales/Tesoro.java
+++ Tema 7/Arrays Bidimensionales/Tesoro.java
@@ -7,8 +7,9 @@ public class Tesoro {
     final int VACIO = 0;
     final int MINA = 1;
     final int TESORO = 2;
-    final int INTENTO = 3;
+    final int INTENTO = 3;//3 intentos
     
+    //Declaración del array y coordenadas
     int x;
     int y;
     int[][] cuadrante = new int[5][4];
@@ -28,20 +29,20 @@ public class Tesoro {
     // coloca el tesoro
     int tesoroX;
     int tesoroY;
-    do {
+    do { //Sigue intentando hasta que no coincidan
       tesoroX = (int)(Math.random() * 5);
       tesoroY = (int)(Math.random() * 4);
     } while ((minaX == tesoroX) && (minaY == tesoroY));
     cuadrante[tesoroX][tesoroY] = TESORO;
     
-    // juego
-    System.out.println("¡BUSCA EL TESORO!");
+    // Juego
+    System.out.println("¡Busca el tesoro! Intenta adivinar donde se encuentra");
     boolean salir = false;
     String c = "";
     do {
       // pinta el cuadrante
       for(y = 3; y >= 0; y--) {
-        System.out.print(y + "|");  
+        System.out.print(y+1 + "|");  
         for(x = 0; x < 5; x++) {
           if (cuadrante[x][y] == INTENTO) {
             System.out.print("X ");
@@ -51,21 +52,21 @@ public class Tesoro {
         }
         System.out.println();
       }
-      System.out.println("  ----------\n  0 1 2 3 4\n");
+      System.out.println("  ----------\n  1 2 3 4 5\n");
 
       // pide las coordenadas  
       System.out.print("Coordenada x: ");
-      x = Integer.parseInt(System.console().readLine());
+      x = Integer.parseInt(s.nextLine())-1;
       System.out.print("Coordenada y: ");
-      y = Integer.parseInt(System.console().readLine());
+      y = Integer.parseInt(s.nextLine())-1;
       
-      // mira lo que hay en las coordenadas indicadas por el usuario
+      //Mira que hay en las coordenadas introducidas
       switch(cuadrante[x][y]) {
         case VACIO:
           cuadrante[x][y] = INTENTO;
           break;
         case MINA:
-          System.out.println("Lo siento, has perdido.");
+          System.out.println("Mala suerte, has perdido.");
           salir = true;
           break;
         case TESORO:
@@ -99,6 +100,6 @@ public class Tesoro {
       }
       System.out.println();    
     }
-    System.out.println("  ----------\n  0 1 2 3 4\n");
+    System.out.println("  ----------\n  1 2 3 4 5\n");
   }
 }
