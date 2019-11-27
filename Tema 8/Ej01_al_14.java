c30b1534500f4f206d882eefbe68240e20afd453
Pedro Torres <torrespedrobarba@gmail.com>
2019-11-27 09:05:49 +0100

    Añadido un ejercicio del tema 8

---
+++ Tema 8/Ej01_al_14.java
@@ -0,0 +1,17 @@
+package matematicas;
+
+public class Mates {
+  public static boolean esPrimo(int num) {
+    return esPrimo((long) n);
+    
+    //2. Es primo
+    if (matematicas.Mates.esPrimo(29)) {
+      System.out.println("El 79 es primo");
+    }
+
+    if (matematicas.Mstes.esPrimo(80)) {
+      System.out.println("El 120 es primo");
+    }
+    
+  }
+}
---
+++ Tema 8/matematicas/Matematicas.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Matematicas {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+  }
+}
